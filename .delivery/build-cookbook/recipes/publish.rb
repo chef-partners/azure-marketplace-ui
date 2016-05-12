ae69983c5343174ff9a4c121e9628f745206651d
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-05-12 12:47:22 -0700

    Set the output_dir correctly

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,13 +4,13 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-base_dir = File.expand_path("../../../")
+output_dir = File.expand_path(File.join(__FILE__, "../../.."))
 
 # Recipe to ensure that the extension for all types is built in the correct directory
 node['delivery']['extension_type'].each do |type|
 
   # determine the directory that the artifacts should be created in
-  artifact_dir = File.join(base_dir, type)
+  artifact_dir = File.join(output_dir, type)
 
   # Determine the output path for the CreateUIDefinition
   createuiddefinition_path = File.join(artifact_dir, 'Artifacts', 'CreateUIDefinition.json')
@@ -18,11 +18,12 @@ node['delivery']['extension_type'].each do |type|
   # Ensure that the directory exists for the path
   directory "#{type}-artifacts" do
     path File.dirname(createuiddefinition_path)
+    recursive true
   end
 
   # Write out the CreateUIDefinition file
   template "create-ui-definition" do
-    path
+    path createuiddefinition_path
     source "CreateUIDefinition.json.erb"
   end
 
