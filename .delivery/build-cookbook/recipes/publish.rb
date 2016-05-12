d639b57d3594209f4b4369a071e8d3ddf84c825e
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-05-12 12:42:38 -0700

    Removed directory creation from default Added the correct path for the directory creation into the publish recipe

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-base_dir = File.expand_path("#{node['delivery']['workspace']['repo']}")
+base_dir = File.expand_path("../../../")
 
 # Recipe to ensure that the extension for all types is built in the correct directory
 node['delivery']['extension_type'].each do |type|
@@ -12,9 +12,17 @@ node['delivery']['extension_type'].each do |type|
   # determine the directory that the artifacts should be created in
   artifact_dir = File.join(base_dir, type)
 
+  # Determine the output path for the CreateUIDefinition
+  createuiddefinition_path = File.join(artifact_dir, 'Artifacts', 'CreateUIDefinition.json')
+
+  # Ensure that the directory exists for the path
+  directory "#{type}-artifacts" do
+    path File.dirname(createuiddefinition_path)
+  end
+
   # Write out the CreateUIDefinition file
   template "create-ui-definition" do
-    path File.join(artifact_dir, 'Artifacts', 'CreateUIDefinition.json')
+    path
     source "CreateUIDefinition.json.erb"
   end
 
