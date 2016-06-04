73080b642c0c1fc31813add2eeb04be95999c634
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2016-06-04 09:51:24 +0100

    Updated recipe and templates so that an extension for both Classic and ARM is produced Added the version of the extension so the Resources file so that it can be seen in the portal

--- .delivery/build-cookbook/attributes/default.rb
+++ .delivery/build-cookbook/attributes/default.rb
@@ -3,7 +3,8 @@
 default['extension']['platforms']["linux"] = {
   "type" => "LinuxChefClient",
   "resource_name" => "LinuxChefExtension",
-  "categories" => ["compute-vmextension-linux"],
+  "category_name" = "compute-vmextension-linux",
+  "categories" => [],
   "filters" => [
   ]
 }
@@ -11,13 +12,41 @@ default['extension']['platforms']["linux"] = {
 default['extension']['platforms']["windows"] = {
   "type" => "ChefClient",
   "resource_name" => "WindowsChefExtension",
-  "categories" => ["compute-vmextension-windows"],
+  "category_name" = "compute-vmextension-windows",
+  "categories" => [],
   "filters" => [
   ]
 }
 
+# Set the models within azure that the extension is to be used for
+default['extension']['models'] = {
+  "arm" => {
+    "suffix" => "-arm",
+    "tag" => "",
+    "extension" => "Microsoft_Azure_Compute",
+    "handler" => "Microsoft.Compute.VmExtension",
+    "type" => "Microsoft.Compute",
+    "api-version" => "2015-06-15",
+    "typeHandlerVersion" => "1210.12",
+    "autoUpgradeMinorVersion" => true,
+    "settings" => "settings",
+    "protected" => "protectedSettings"
+  },
+  "classic" => {
+    "suffix" => "",
+    "tag" => "classic",
+    "extension" => "Microsoft_Azure_Classic_Compute",
+    "handler" => "Microsoft.ClassicCompute.VmExtension",
+    "type" => "Microsoft.ClassicCompute",
+    "api-version" => "2015-06-01",
+    "version" => "1*".
+    "settings" => "parameters.public",
+    "protected" => "parameters.private"
+  }
+}
+
 # Set the version number of the extension
-default['extension']['version'] = "1.1.0"
+default['extension']['version'] = "1.2.0"
 
 # Define the list if icons that need to be written out
 default['extension']['icons'] = [
--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -6,99 +6,123 @@
 
 require 'json'
 
-output_dir = node['extension']['output']['dir']
+# Iterate around the models
+node['extension']['models'].each do |model, info|
 
-# Recipe to ensure that the extension for all types is built in the correct directory
-node['extension']['platforms'].each do |platform, details|
+  output_dir = File.join(node['extension']['output']['dir'], model)
 
-  # determine the directory that the artifacts should be created in
-  artifact_dir = File.join(output_dir, platform)
+  # Recipe to ensure that the extension for all types is built in the correct directory
+  node['extension']['platforms'].each do |platform, details|
 
-  # determine the output locations
-  outputs = {
-    "createuidefinition" => File.join(artifact_dir, 'Artifacts', 'CreateUIDefinition.json'),
-    "maintemplate" => File.join(artifact_dir, 'Artifacts', 'MainTemplate.json'),
-    "manifest" => File.join(artifact_dir, "Manifest.json"),
-    "stringresources" => File.join(artifact_dir, "Strings", "Resources.resjson"),
-    "uidefinition" => File.join(artifact_dir, "UIDefinition.json"),
-  }
+    # determine the directory that the artifacts should be created in
+    artifact_dir = File.join(output_dir, platform)
 
-  # Ensure that the directory exists for the path
-  directory "#{platform}-artifacts-dir" do
-    path File.dirname(outputs["createuidefinition"])
-    recursive true
-  end
+    # determine the output locations
+    outputs = {
+      "createuidefinition" => File.join(artifact_dir, 'Artifacts', 'CreateUIDefinition.json'),
+      "maintemplate" => File.join(artifact_dir, 'Artifacts', 'MainTemplate.json'),
+      "manifest" => File.join(artifact_dir, "Manifest.json"),
+      "stringresources" => File.join(artifact_dir, "Strings", "Resources.resjson"),
+      "uidefinition" => File.join(artifact_dir, "UIDefinition.json"),
+    }
 
-  directory "#{platform}-icons-dir}" do
-    path File.join(artifact_dir, 'Icons')
-    recursive true
-  end
-
-  directory "#{platform}-strings-dir}" do
-    path File.dirname(outputs["stringresources"])
-    recursive true
-  end
-
-  # Write out the CreateUIDefinition file
-  template "create-ui-definition" do
-    path outputs["createuidefinition"]
-    source "Artifacts/CreateUIDefinition.json.erb"
-    variables({
-        "platform" => platform
-    })
-  end
-
-  # Main Template
-  template "create-maintemplate" do
-    path outputs["maintemplate"]
-    source "Artifacts/MainTemplate.json.erb"
-    variables({
-        "platform" => platform,
-        "details" => details
-    })
-  end
-
-  # write out the manifest file
-  template "manifest" do
-    path outputs["manifest"]
-    source "Manifest.json.erb"
-    variables({
-        "platform" => platform,
-        "details" => details
-    })
-  end
-
-  # write out the icons
-  node['extension']['icons'].each do |icon|
-    cookbook_file icon do
-      source "Icons/#{icon}"
-      path File.join(artifact_dir, 'Icons', icon)
+    # Ensure that the directory exists for the path
+    directory "#{platform}-artifacts-dir" do
+      path File.dirname(outputs["createuidefinition"])
+      recursive true
     end
-  end
 
-  # Strings file
-  template "#{platform}-strings" do
-    path outputs["stringresources"]
-    source "Strings/Resources.resjson.erb"
-    variables({
-        "platform" => platform
-    })
-  end
+    directory "#{platform}-icons-dir}" do
+      path File.join(artifact_dir, 'Icons')
+      recursive true
+    end
 
-  # Write out the UIDefinition file
-  cookbook_file "#{platform}-uidefinition" do
-    path outputs["uidefinition"]
-    source "UIDefinition.json"
-  end
+    directory "#{platform}-strings-dir}" do
+      path File.dirname(outputs["stringresources"])
+      recursive true
+    end
 
-  # Noe that all the files have been generated for each of the platforms execute the zip
-  # command to make zip file archives out of them
-  # generate the output filename
-  archive_file_path = File.join(output_dir, "azure-marketplace-ui-#{platform}-#{node['extension']['version']}.zip")
-  bash "#{platform}-zip-archive" do
-    cwd output_dir
-    code <<-EHO
-      zip -9 -r #{archive_file_path} #{platform}
-    EHO
+    # Write out the CreateUIDefinition file
+    template "create-ui-definition" do
+      path outputs["createuidefinition"]
+      source "Artifacts/CreateUIDefinition.json.erb"
+      variables({
+          "platform" => platform,
+          "info" => info
+      })
+    end
+
+    # Main Template
+    template "create-maintemplate" do
+      path outputs["maintemplate"]
+      source "Artifacts/MainTemplate.json.erb"
+      variables({
+          "platform" => platform,
+          "details" => details,
+          "info" => info,
+          "model" => model
+      })
+    end
+
+    # Determine the categories for the model
+    # This is based on whatever has been specified in the array as well as the
+    # category name
+    case model do
+    when "arm"
+      categories << details['category_name']
+    when "classic"
+      categories << info['tag'] + details['category_name'].capitalize
+    end
+
+    # write out the manifest file
+    template "manifest" do
+      path outputs["manifest"]
+      source "Manifest.json.erb"
+      variables({
+          "model" => model,
+          "info" => info,
+          "platform" => platform,
+          "details" => details,
+          "categories" => categories
+      })
+    end
+
+    # write out the icons
+    node['extension']['icons'].each do |icon|
+      cookbook_file icon do
+        source "Icons/#{icon}"
+        path File.join(artifact_dir, 'Icons', icon)
+      end
+    end
+
+    # Strings file
+    template "#{platform}-strings" do
+      path outputs["stringresources"]
+      source "Strings/Resources.resjson.erb"
+      variables({
+          "platform" => platform,
+          "version" => node['extension']['version']
+      })
+    end
+
+    # Write out the UIDefinition file
+    template "#{platform}-uidefinition" do
+      path outputs["uidefinition"]
+      source "UIDefinition.json"
+      variables({
+        "extension" => info["extension"]
+      })
+    end
+
+    # Noe that all the files have been generated for each of the platforms execute the zip
+    # command to make zip file archives out of them
+    # generate the output filename
+    archive_file_path = File.join(output_dir, "azure-marketplace-ui-#{platform}#{info['suffix']}.#{node['extension']['version']}.zip")
+    bash "#{platform}-zip-archive" do
+      cwd output_dir
+      code <<-EHO
+        zip -9 -r #{archive_file_path} #{platform}
+      EHO
+    end
   end
 end
